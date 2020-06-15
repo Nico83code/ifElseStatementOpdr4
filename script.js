696ce5f669aa297947c14298d5003f583e18ac1e
nico83code <nico.man.codaisseur@gmail.com>
2020-06-15 23:36:16 +0200

    if else statement opdr4

---
+++ script.js
@@ -0,0 +1,23 @@
+//deel 1
+const age = 15;
+if (age >= 18) {
+  return console.log("Je mag naar binnen");
+} else {
+  console.log("Je bent te jong, je mag nog niet naar binnen");
+}
+
+//deel 2
+const isFemale = true;
+if (isFemale) {
+  return console.log("Dit is waar");
+} else {
+  console.log("Dit is niet waar");
+}
+
+//deel 3
+const driverStatus = "bob";
+if (driverStatus === "bob") {
+  return console.log("Je bent bob, je mag rijden");
+} else {
+  console.log("Je bent geen bob, je mag niet rijden");
+}
